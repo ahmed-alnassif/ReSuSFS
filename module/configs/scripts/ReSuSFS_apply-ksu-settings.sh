165154e3e22a5e62a3d86a424c7f423df3b40f54
Ahmed Al-Nassif <mr.ahmed.nassif@gmail.com>
2026-09-04 17:28:07 +0300

    module(fix): set ksu features correctly
    Signed-off-by: Ahmed Al-Nassif <mr.ahmed.nassif@gmail.com>

--- module/configs/scripts/ReSuSFS_apply-ksu-settings.sh
+++ module/configs/scripts/ReSuSFS_apply-ksu-settings.sh
@@ -5,9 +5,8 @@
 
 PATH=/data/adb/ksu/bin:/data/data/com.termux/files/usr/bin:$PATH
 
-ksud feature set su_compat 1
-ksud feature set kernel_umount 1
-ksud feature set selinux_hide 1
+ksud feature set 1 0
+ksud feature set 4 1
 
 echo "[+] features set successfully"
 echo "[*] no reboot required"
