6f936103a08ba83e7754a0dfd3dcd1a36cc5a826
Ahmed Al-Nassif <mr.ahmed.nassif@gmail.com>
2026-09-15 10:41:04 +0300

    module: ask for configs backup only when they exist
    Signed-off-by: Ahmed Al-Nassif <mr.ahmed.nassif@gmail.com>

--- module/customize.sh
+++ module/customize.sh
@@ -28,11 +28,13 @@ detect_key_press() {
 	fi
 }
 
-ui_print "[*] Have you backed up your configuration?"
-ui_print "[*] VOLUME UP for YES, DOWN for NO"
-if ! detect_key_press; then
-	ui_print "[!] Please back up your configuration first"
-	exit 1
+if [ -d "$PERSISTENT_DIR" ]; then
+	ui_print "[*] Have you backed up your configuration?"
+	ui_print "[*] VOLUME UP for YES, DOWN for NO"
+	if ! detect_key_press; then
+		ui_print "[!] Please back up your configuration first"
+		exit 1
+	fi
 fi
 
 CONFIG_DIR="$MODPATH/configs"
